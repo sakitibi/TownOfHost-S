af1a473dabd3eae5bdae710c41bbb2fce619663e
tukasa0001 <tts13511@hotmail.com>
2022-02-09 00:26:01 +0900

    ログを削除

--- Modules/CustomCosmeticsManager.cs
+++ Modules/CustomCosmeticsManager.cs
@@ -14,29 +14,44 @@ using System.Linq;
 namespace TownOfHost
 {
     static class CustomNamePlatesManager {
+        public static List<NamePlateData> CustomPlates;
+        public static bool isAdded = false;
         public static void LoadAllPlates() {
             var plateDir = new DirectoryInfo("NamePlates");
-            var jsonFiles = plateDir.GetFiles("*.json");
-            foreach(var file in jsonFiles) {
+            var pngFiles = plateDir.GetFiles("*.png");
+            CustomPlates = new List<NamePlateData>();
+            foreach(var file in pngFiles) {
+                try {
+                    var plate = ScriptableObject.CreateInstance<NamePlateData>();
+                    plate.name = file.Name.Substring(0, file.Name.Length - 5);
+                    plate.ProductId = "TOH_" + file.Name;
+                    plate.Order = 99;
+                    plate.ChipOffset = new Vector2(0f, 0.2f);
+                    plate.Free = true;
+                    plate.Image = TextureManager.loadSprite("NamePlates\\" + file.Name);
+                    CustomPlates.Add(plate);
+                    Logger.info("プレート読み込み完了:" + file.Name);
+                }
+                catch {
+                    Logger.error("エラー:CustomNamePlateの読み込みに失敗しました:" + file.FullName);
+                }
             }
+            Logger.info("プレート読み込み処理終了");
+            isAdded = false;
         }
     }
     [HarmonyPatch(typeof(HatManager), nameof(HatManager.GetUnlockedNamePlates))]
     class GetUnlockedNamePlatesPatch {
-        static bool isAdded = false;
         public static void Postfix(HatManager __instance, ref Il2CppReferenceArray<NamePlateData> __result) {
-            if(isAdded) return;
+            if(CustomNamePlatesManager.isAdded) return;
             var AllPlates = __result.ToList();
-            var plate = ScriptableObject.CreateInstance<NamePlateData>();
-            plate.name = "CustomPlate";
-            plate.Order = 99;
-            plate.ChipOffset = new Vector2(0f, 0.2f);
-            plate.Free = true;
-            plate.Image = TextureManager.loadSprite("Resources\\screenshot.png");
-            AllPlates.Add(plate);
+            foreach(var plate in CustomNamePlatesManager.CustomPlates) {
+                AllPlates.Add(plate);
+                __instance.AllNamePlates.Add(plate);
+            }
+
             __result = AllPlates.ToArray();
-            __instance.AllNamePlates.Add(plate);
-            isAdded = true;
+            CustomNamePlatesManager.isAdded = true;
         }
     }
 }
