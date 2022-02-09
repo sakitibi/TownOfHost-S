eb62284606d2be4d4d8a647e73d1aa1ecc9d6b63
tukasa0001 <tts13511@hotmail.com>
2022-02-09 14:31:49 +0900

    プレートのデータ読み込み処理と変数をGetUnlockedNamePlatesPatchに移動

--- Modules/CustomCosmeticsManager.cs
+++ Modules/CustomCosmeticsManager.cs
@@ -13,13 +13,19 @@ using System.Linq;
 
 namespace TownOfHost
 {
-    static class CustomNamePlatesManager {
-        public static List<NamePlateData> CustomPlates;
+    [HarmonyPatch(typeof(HatManager), nameof(HatManager.GetUnlockedNamePlates))]
+    class GetUnlockedNamePlatesPatch {
         public static bool isAdded = false;
-        public static void LoadAllPlates() {
+        public static void Postfix(HatManager __instance, ref Il2CppReferenceArray<NamePlateData> __result) {
+            if(isAdded) return;
+            isAdded = true;
+            Logger.info("プレート読み込み処理開始");
+            var AllPlates = __result.ToList();
+
             var plateDir = new DirectoryInfo("NamePlates");
+            if(!plateDir.Exists) plateDir.Create();
             var pngFiles = plateDir.GetFiles("*.png");
-            CustomPlates = new List<NamePlateData>();
+            var CustomPlates = new List<NamePlateData>();
             foreach(var file in pngFiles) {
                 try {
                     var plate = ScriptableObject.CreateInstance<NamePlateData>();
@@ -32,6 +38,8 @@ namespace TownOfHost
                     plate.Free = true;
                     plate.Image = TextureManager.loadSprite("NamePlates\\" + file.Name);
                     CustomPlates.Add(plate);
+                    AllPlates.Add(plate);
+                    __instance.AllNamePlates.Add(plate);
                     Logger.info("プレート読み込み完了:" + file.Name);
                 }
                 catch {
@@ -39,22 +47,8 @@ namespace TownOfHost
                 }
             }
             Logger.info("プレート読み込み処理終了");
-            //isAdded = false;
-        }
-    }
-    [HarmonyPatch(typeof(HatManager), nameof(HatManager.GetUnlockedNamePlates))]
-    class GetUnlockedNamePlatesPatch {
-        public static void Postfix(HatManager __instance, ref Il2CppReferenceArray<NamePlateData> __result) {
-            if(CustomNamePlatesManager.isAdded) return;
-            CustomNamePlatesManager.LoadAllPlates();
-            var AllPlates = __result.ToList();
-            foreach(var plate in CustomNamePlatesManager.CustomPlates) {
-                AllPlates.Add(plate);
-                __instance.AllNamePlates.Add(plate);
-            }
 
             __result = AllPlates.ToArray();
-            CustomNamePlatesManager.isAdded = true;
         }
     }
     [HarmonyPatch(typeof(CosmeticData), nameof(CosmeticData.GetItemName))]
