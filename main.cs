2670b690ff424ea16beb6cb793570bc036e0951f
shu-TownofHost <itozakishunnya@gmail.com>
2022-02-16 20:20:25 +0900

    問題修正中

--- main.cs
+++ main.cs
@@ -828,13 +828,12 @@ namespace TownOfHost
                 main.witchMeeting = false;
             }
     {
-                private static void CustomSyncAllSettings()
-                {
-                    foreach (var pc in PlayerControl.AllPlayerControls)
-                    {
-                        pc.CustomSyncSettings();
-                    }
-                }
+        private static void CustomSyncAllSettings(){
+            foreach (var pc in PlayerControl.AllPlayerControls)
+            {
+                pc.CustomSyncSettings();
+            }
+        }
         public override void Load()
         {
             TextCursorTimer = 0f;
